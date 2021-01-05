c96a4e13f7880a9cd6e49a5044547d2f473308ae
madiha-eman <madihaema1@gmail.com>
2021-01-05 13:22:00 +0500

    'web'

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const  app = express()
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.listen(PORT, (req,res) =>{
     console.log('server is runing at:', PORT)
@@ -25,12 +25,43 @@ app.listen(PORT, (req,res) =>{
 //     app.use('/public', express.static('public'))
 //     res.sendFile(path.join(__dirname, 'public', 'Contactus.html'))
 // })
+let user = [
+    {name:'madiha', email:'madi@gmail.com',password: '2233', id:1 },
+    {name:'asma', email:'asso@gmail.com',password: '2211', id:2 }
+
+]
 
 app.use(express.static(path.join(__dirname,'public')))
+app.use('/public', express.static('public'))
+
 
 app.get('/signup',(req,res)=>{
+    app.use('/registration', express.static('registration'))
     res.sendFile(path.join(__dirname, 'registration', 'SignUp.html'))
 })
 app.post('/signup',(req,res)=>{
-    res.send(req.body)
+    let {name,email,password} = req.body
+    let found = user.some((item)=>item.email == email)
+    if(found){
+        res.send('user already existed')
+    } else{
+        user.push({email,name,password,id:user.length+1})
+        res.redirect('/login')
+    
+    }
+})
+app.get('/login',(req,res)=>{
+    app.use('/registration', express.static('registration'))
+    res.sendFile(path.join(__dirname, 'registration', 'Login.html'))
+})
+app.post('/login', (req,res)=>{
+    let {name,email,password} = req.body
+    let found1 = user.some((item)=>item.email == email && item.password == password)
+    if(found1){
+        res.redirect('/')
+    }
+    else{
+        res.send('not correct email or password')
+    }
+
 })
